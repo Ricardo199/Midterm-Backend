f204206a8beb01fca390a3b2623283b73c1ddcd9
Ricardo199 <ricardo1529@gmail.com>
2024-10-29 19:53:26 -0400

    Add dotenv support and update MongoDB connection string in config

--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
+require('dotenv').config({ path: 'secrets.env' });
+const connectionString = `mongodb+srv://ricardo1529:${process.env.ATLAS_DB_PASSWORD}@midterm.fuok2.mongodb.net/?retryWrites=true&w=majority&appName=Midterm`;
 module.exports = {
-    "ATLASDB":"mongodb+srv://ricardo1529:Cs5y6Hm7LClWNe1C@midterm.fuok2.mongodb.net/?retryWrites=true&w=majority&appName=Midterm"
-}
+    "ATLASDB": connectionString
+};
