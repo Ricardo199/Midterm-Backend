1954e6de292c5fe4d4593f11f5222fb4205d54f3
Ricardo199 <ricardo1529@gmail.com>
2024-10-29 20:31:59 -0400

    Update dotenv config path to use relative directory for secrets.env

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: 'secrets.env' });
+require('dotenv').config({ path: './config/secrets.env' });
 const connectionString = `mongodb+srv://ricardo1529:${process.env.ATLAS_DB_PASSWORD}@midterm.fuok2.mongodb.net/?retryWrites=true&w=majority&appName=Midterm`;
 module.exports = {
     "ATLASDB": connectionString
